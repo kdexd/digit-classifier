4c1dc30d216e6f248b8a76a259c0f04cb8362ea8
Karan Desai <karandesai_96@live.com>
2015-10-21 14:08:40 +0530

    defined NeuralNetwork class having weights, biases and sizes of layers

---
+++ NeuralNetwork.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class NeuralNetwork (object):
+
+    def __init__ (self, sizes):
+        # sizes is a list of neurons in successive layers
+
+        # the number of layers in the network is equal to length of 'sizes' list
+        self.numberOfLayers = len(sizes)
+
+        self.sizes = sizes
+
+        # random initialization of weights/ biases for a starting to gradient descent
+        # random generation is through guassian distribution
+        # random numbers will have mean 0 and std dev 1
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
