b7d3fe321029d3dfa501a6447627a272861fd303
Karan Desai <karandesai_96@live.com>
2016-08-26 23:25:22 +0530

    Add an io utility to save model as numpy compressed binary.

---
+++ io_utils/model_save_wrapper.py
@@ -0,0 +1,18 @@
+"""
+Wrapper module to save the weights and biases of a particular network model
+as compressed numpy binaries.
+"""
+import os
+import numpy as np
+
+model_dirpath = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), os.pardir, 'models'
+)
+
+
+def save_model(network, dirpath=model_dirpath, filename='model.npz'):
+    np.savez_compressed(
+        file=os.path.join(dirpath, filename),
+        weights=network.weights,
+        biases=network.biases
+    )
