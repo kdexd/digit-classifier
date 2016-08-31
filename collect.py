780c1d38dc9accaa3770a0230897470829ee6cc8
Karan Desai <karandesai_96@live.com>
2016-08-31 22:12:38 +0530

    Simplify mnist dataset wrapper script.

--- collect.py
+++ collect.py
@@ -1,28 +1,26 @@
 import os
 import gzip
 import cPickle
+import wget
 
 import numpy as np
 
 
-def load_mnist_gz():
-    data_file = gzip.open(os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), os.pardir, "data", "mnist.pkl.gz"), "rb")
+def load_mnist():
+    if not os.path.exists(os.path.join(os.curdir, 'data')):
+        os.mkdir(os.path.join(os.curdir, 'data'))
+        wget.download('http://deeplearning.net/data/mnist/mnist.pkl.gz', out='data')
 
+    data_file = gzip.open(os.path.join(os.curdir, 'data', 'mnist.pkl.gz'), 'rb')
     training_data, validation_data, test_data = cPickle.load(data_file)
     data_file.close()
-    return training_data, validation_data, test_data
-
-
-def load_data_wrapper():
-    training_data, validation_data, test_data = load_mnist_gz()
 
     training_inputs = [np.reshape(x, (784, 1)) for x in training_data[0]]
     training_results = [vectorized_result(y) for y in training_data[1]]
     training_data = zip(training_inputs, training_results)
 
     validation_inputs = [np.reshape(x, (784, 1)) for x in validation_data[0]]
-    validation_results = [vectorized_result(y) for y in validation_data[1]]
+    validation_results = validation_data[1]
     validation_data = zip(validation_inputs, validation_results)
 
     test_inputs = [np.reshape(x, (784, 1)) for x in test_data[0]]
